6bd7b7b8414e2af0e0065b698aa34202a5aebe99
Kadetete <g.cadete.m@gmail.com>
2024-07-16 11:18:11 -0300

    adição de componente btn

--- App.tsx
+++ App.tsx
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import Btn from './components/Btn'
+import Display from './components/Display'
+
 
 export default function App() {
-  const [calc, setCalc] = useState(0)
+  const [num, setNum] = useState(0)
+
+  const dec = () => {
+    setNum(num - 1)
+  }
+
+  const inc = () => {
+    setNum(num + 1)
+  }
+
   return (
+    <>
     <View style={styles.container}>
-      
+      <Text style={styles.paragraph}>
+        Contador
+      </Text>
+      <Display valor={num}/>
     </View>
+
+    <View style={styles.btns}>
+        <Btn style={styles.btn}
+             label="-" 
+             onClick={dec} 
+             />
+        <Btn label="+" 
+             onClick={inc} 
+             style={styles.btn}/>
+      </View> 
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
   },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  btns: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between'
+  },
+  btn: {
+    width: 20,
+  }
 });
+
---
+++ components/Btn.tsx
@@ -0,0 +1,27 @@
+import { Text, StyleSheet, TouchableHighlight, Dimensions } from 'react-native';
+
+export default function Btn(props: any) {
+  return (
+    <TouchableHighlight 
+    style={styles.container} 
+    onPress={props.onClick}>
+      <Text style={styles.txt}>{props.label}</Text>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#00f'
+  },
+  txt: {
+    fontSize: 30,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: Dimensions.get('window').width / 4
+  }
+});
